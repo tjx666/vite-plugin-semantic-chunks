a1daafc7e6751a724fb433d3ea4e612f8c903a31
changfeng <changfeng@gaoding.com>
2023-06-03 05:00:39 +0800

    feat: basic implementation

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,71 @@
-import path from 'node:path';
-
 import { readPackageUpSync } from 'read-pkg-up';
 import type { Plugin } from 'vite';
 
-function getLongerChunkPath(facadeModuleId: string | null) {
+function getChunkPkgName(facadeModuleId: string | null) {
     if (!facadeModuleId) return '';
 
-    const latestPackageJSON = readPackageUpSync({ cwd: facadeModuleId });
-    const dirName = path.basename(path.dirname(facadeModuleId));
-    const pkgName = latestPackageJSON?.packageJson.name.replace(/\//, '+');
-    return `${pkgName}+${dirName}+`;
+    const closestPkg = readPackageUpSync({ cwd: facadeModuleId });
+    if (!closestPkg) return '';
+
+    return `${closestPkg.packageJson.name.replace(/\//, '+')}+`;
 }
 
 const assetExtToFolderMap = new Map([
     [/.(jpe?g|a?png)$/, 'image'],
     [/.(mp4|mov|ts|flv|avi)$/, 'video'],
     [/.(mp3|aac|wav|flac)$/, 'audio'],
+    [/.(ttf|woff|woff2|otf|eot)$/, 'font'],
 ]);
-function getAssetFolder(assetFileName: string | undefined) {
-    let assetFolder = '[ext]';
-    if (assetFileName) {
-        for (const [reg, folder] of assetExtToFolderMap.entries()) {
-            if (reg.test(assetFileName)) {
-                assetFolder = folder;
-                break;
-            }
-        }
-    }
-    return `${assetFolder}/[name]-[hash][extname]`;
-}
 
-export function vitePluginSematicChunks(): Plugin {
+export default function vitePluginSematicChunks(): Plugin {
     return {
         name: 'vite-plugin-sematic-chunks',
         config() {
             return {
+                worker: {
+                    rollupOptions: {
+                        output: {
+                            entryFileNames(chunkInfo) {
+                                const pkgNameAndDir = getChunkPkgName(chunkInfo.facadeModuleId);
+                                return `js/worker/${pkgNameAndDir}[name]-[hash].js.js`;
+                            },
+                        },
+                    },
+                },
                 build: {
                     rollupOptions: {
                         output: {
-                            assetFileNames(assetInfo) {
-                                return getAssetFolder(assetInfo.name);
+                            assetFileNames(chunkInfo) {
+                                const assetFileName = chunkInfo.name;
+                                let assetFolder = '[ext]';
+                                if (assetFileName) {
+                                    for (const [reg, folder] of assetExtToFolderMap.entries()) {
+                                        if (reg.test(assetFileName)) {
+                                            assetFolder = folder;
+                                            break;
+                                        }
+                                    }
+                                }
+                                return `${assetFolder}/[name]-[hash][extname]`;
                             },
                             chunkFileNames(chunkInfo) {
                                 const namespaces: string[] = [];
+                                if (chunkInfo.isDynamicEntry) {
+                                    namespaces.push('dynamic');
+                                } else {
+                                    namespaces.push('chunks');
+                                }
+
                                 if (chunkInfo.isImplicitEntry) {
                                     namespaces.push('implicit');
                                 }
-                                if (chunkInfo.isDynamicEntry) {
-                                    namespaces.push('dynamic');
-                                }
-                                const chunksFolder =
-                                    namespaces.length > 0 ? namespaces.join('-') : 'chunks';
-                                const chunkPkg = getLongerChunkPath(chunkInfo.facadeModuleId);
-                                return `js/${chunksFolder}/${chunkPkg}[name]-[hash].js`;
+
+                                const chunksFolder = namespaces.join('-');
+                                const chunkPkgName = getChunkPkgName(chunkInfo.facadeModuleId);
+                                return `js/${chunksFolder}/${chunkPkgName}[name]-[hash].js`;
                             },
                             entryFileNames() {
-                                return 'main-[hash].js';
+                                return `[name]-[hash].js`;
                             },
                         },
                     },
